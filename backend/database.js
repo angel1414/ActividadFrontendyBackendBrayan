bc8f8299e8d735501c4f4b024bdee5b8616bf62c
Angel <angelfan1637@gmail.com>
2025-05-29 14:10:42 -0600

    cambio 2

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // 1- Configuro la URI o dirección de la base de datos
-const URI = "mongodb://localhost:27017/ZonaDigitalDB20170508";
+const URI = "mongodb+srv://fito:WYBS0lQxFDP31IYm@sitiowebcholos.w6tdj.mongodb.net/CineMarck?retryWrites=true&w=majority&appName=SitioWebCholos";
 
 // 2- Conecto la base de datos
 mongoose.connect(URI);
